0b9321a98eb38789fd625012f65a12292be466c0
faruk6276 <hilton627681@gmail.com>
2021-04-10 02:46:51 +0600

    Filters with yesterday

--- history/views.py
+++ history/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from .models import History    
 # Create your views here.
+def index(request):
+    total_data = History.objects.all()
+    k={}
+    for value in total_data:
+        if value.keywords in k:
+            k[value.keywords]+=1
+        else:
+            k[value.keywords]=1
+    u=set()
+    for value in total_data:
+        if value.user not in u:
+            u.add(value.user)
+    return render(request,'home1.html',{'total_data': total_data,'total_keywords':k,'users':u})
